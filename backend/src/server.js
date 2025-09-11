1e05c4cc64e0a45c063f21bb8204e5928f7f034b
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-09-11 15:55:18 +0700

    fix: redo `app.listen`

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,10 @@ import connectDB from "./services/db.js";
 
 const PORT = process.env.PORT || 39399;
 
-connectDB()
-  .then(() => {
-    if (!process.env.OPENROUTER_API_KEY) {
-      throw Error("No OpenRouter API key supplied.");
-    }
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Backend Server ready at http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Failed to start server due to DB error:", err);
-    process.exit(1);
-  });
+app.listen(PORT, "0.0.0.0", () => {
+  if (!process.env.OPENROUTER_API_KEY) {
+    throw Error("No OpenRouter API key supplied.");
+  }
+  connectDB();
+  console.log(`Backend Server ready at http://localhost:${PORT}`);
+});
