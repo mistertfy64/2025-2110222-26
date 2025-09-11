f016e1eb3ebcc7bbb87a32c343fe0eea6f32f6a5
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-09-11 20:40:02 +0700

    feature: add chat modify modal

---
+++ frontend/public/js/modification-menu.js
@@ -0,0 +1,60 @@
+async function openSessionModificationMenu(sessionID) {
+  const data = await fetch(`${API_BASE}/api/sessions/${sessionID}`);
+  const dataJSON = await data.json();
+  initializeModificationDialog(dataJSON);
+  const dialog = document.getElementById("modification-menu");
+  dialog.showModal();
+}
+
+function initializeModificationDialog(dataJSON) {
+  document.getElementById("modification-menu__session-to-change").value =
+    dataJSON.sessionId;
+  document.getElementById("modification-menu__new-chat-name").value =
+    dataJSON?.name ?? "";
+  document.getElementById("modification-menu__new-chat-color").value =
+    dataJSON?.color ?? "#1a1a1a";
+  // ...
+  document
+    .getElementById("modification-menu__confirm-button")
+    .addEventListener("click", setNewChatSettings);
+  document
+    .getElementById("modification-menu__close-button")
+    .addEventListener("click", destroyModificationDialog);
+}
+
+// TODO: Add feedback on fetch success/fail (Do something with `result`)
+async function setNewChatSettings() {
+  const sessionID = document.getElementById(
+    "modification-menu__session-to-change"
+  ).value;
+  const newName = document.getElementById(
+    "modification-menu__new-chat-name"
+  ).value;
+  const newColor = document.getElementById(
+    "modification-menu__new-chat-color"
+  ).value;
+  const result = await fetch(`${API_BASE}/api/sessions/${sessionID}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ newName: newName, newColor: newColor })
+  });
+  destroyModificationDialog();
+  // TODO: can we find a better way to do this
+  window.location.reload();
+}
+
+// This is here so no one overwrites a chat session info "on accident"
+function destroyModificationDialog() {
+  document.getElementById("modification-menu__session-to-change").value = "";
+  document.getElementById("modification-menu__new-chat-name").value = "";
+  document.getElementById("modification-menu__new-chat-color").value =
+    "#1a1a1a";
+  document
+    .getElementById("modification-menu__confirm-button")
+    .removeEventListener("click", setNewChatSettings);
+  document
+    .getElementById("modification-menu__close-button")
+    .removeEventListener("click", destroyModificationDialog);
+  const dialog = document.getElementById("modification-menu");
+  dialog.close();
+}
