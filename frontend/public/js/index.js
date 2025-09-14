c1282bc9888f94cc81acbb3fb261b9a228d80598
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-09-14 14:49:53 +0700

    change: change from six to two and image format from png to webp

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -28,7 +28,7 @@ async function init() {
   }
   document.getElementById(
     "character"
-  ).src = `./assets/images/emotions/six-neutral.png`;
+  ).src = `./assets/images/emotions/two-neutral.webp`;
 }
 
 function bindUI() {
@@ -133,7 +133,7 @@ async function selectSession(sessionId) {
   // also reset character pose
   document.getElementById(
     "character"
-  ).src = `./assets/images/emotions/six-neutral.png`;
+  ).src = `./assets/images/emotions/two-neutral.webp`;
 }
 
 async function createNewSession() {
@@ -285,7 +285,7 @@ async function handleSendClicked() {
     const emotion = getEmotionClassification(data.reply.emotion);
     document.getElementById(
       "character"
-    ).src = `./assets/images/emotions/six-${emotion}.png`;
+    ).src = `./assets/images/emotions/two-${emotion}.webp`;
 
     // Prefer to re-fetch full history (keeps UI consistent with server)
     await loadSessions(); // update list (maybe new updatedAt)
